6a83fa62db936ab271a7fae6bdb76c90b3075d85
Ivo Tzvetkov <ivotkv@gmail.com>
2020-06-16 15:01:10 +1000

    support for VS Code on OS X

--- init.sh
+++ init.sh
@@ -10,5 +10,10 @@ source venv/bin/activate
 pip install --upgrade pip
 pip install --upgrade setuptools
 pip install ipython
+pip install flake8
+
+if [ -d /usr/local/opt/openssl/lib ]; then
+    export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/opt/openssl/lib/
+fi
 
 pip install -r requirements.txt
