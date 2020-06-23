9e72b1b448ce2db500099cbb92a8c67cba18ba93
Ivo Tzvetkov <ivotkv@gmail.com>
2020-06-24 09:10:45 +1000

    add db default value functions

--- app/db.py
+++ app/db.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from uuid import uuid4
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import UUID, JSON, JSONB
 from sqlalchemy.exc import SQLAlchemyError
@@ -8,6 +10,8 @@ from sqlalchemy_json import mutable_json_type
 from .app import app
 
 db = SQLAlchemy(app)
+db.now = datetime.now
+db.uuid4 = uuid4
 db.UUID = UUID
 db.JSON = mutable_json_type(dbtype=JSON, nested=True)
 db.JSONB = mutable_json_type(dbtype=JSONB, nested=True)
