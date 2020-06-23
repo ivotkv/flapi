03ab93ef149590e81a8f2d6f077f65e1c5f0224b
Ivo Tzvetkov <ivotkv@gmail.com>
2020-06-23 14:30:52 +1000

    handle JSON mutations

--- app/db.py
+++ app/db.py
@@ -1,14 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.dialects.postgresql import UUID, JSONB
+from sqlalchemy.dialects.postgresql import UUID, JSON, JSONB
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm.attributes import InstrumentedAttribute, flag_modified
 from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
+from sqlalchemy_json import mutable_json_type
 
 from .app import app
 
 db = SQLAlchemy(app)
 db.UUID = UUID
-db.JSONB = JSONB
+db.JSON = mutable_json_type(dbtype=JSON, nested=True)
+db.JSONB = mutable_json_type(dbtype=JSONB, nested=True)
 db.SQLAlchemyError = SQLAlchemyError
 db.InstrumentedAttribute = InstrumentedAttribute
 db.flag_modified = flag_modified
